43d96d075466434bff0d506257f00196254483a3
eliverG <eliver39g@gmail.com>
2022-12-02 20:54:18 -0600

    ADD servicios REST

--- src/main/java/com/bodega/dao/ProductosDaoInt.java
+++ src/main/java/com/bodega/dao/ProductosDaoInt.java
@@ -1,11 +1,10 @@
 package com.bodega.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bodega.model.Productos;
 
 @Repository
-public interface ProductosDaoInt extends JpaRepository<Productos, Integer>{
+public interface ProductosDaoInt extends CrudRepository<Productos, Integer>{
 
 }
--- src/main/java/com/bodega/svc/interfaces/ProductosSvcInt.java
+++ src/main/java/com/bodega/svc/interfaces/ProductosSvcInt.java
@@ -6,7 +6,12 @@ import java.util.Optional;
 import com.bodega.model.Productos;
 
 public interface ProductosSvcInt{
+	
 	public List<Productos>listAllProducts();
+	
 	public void deleteProductById(int idProduct);
+	
 	public Optional<Productos>getProductById(int idProduct);
+	
+	public int saveProduct(Productos p);
 }
